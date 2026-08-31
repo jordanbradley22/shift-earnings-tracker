c71a8036d72a1826dcf787438d59bb6f90a42a58
Jordan Bradley <jordan.bradley22@gmail.com>
2026-08-31 16:37:46 -0500

    Collect and display one work shift

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Shift Earnings Tracker")
+print()
+
+date = input("Enter the shift date: ")
+job = input("Enter the job name: ")
+hours = input("Enter the hours worked: ")
+earnings = input("Enter the amount earned: $"
+
+print()
+print("Shift Summary")
+print(f"Date: {date}")
+print(f"Job: {job}")
+print(f"Hours worked: {hours}")
+print(f"Amount earned: ${earnings}")
